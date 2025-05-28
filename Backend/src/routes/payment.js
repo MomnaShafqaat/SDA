931786874659f6318f5674f694f9359f53b30e98
Momna <momnashafqaat@gmail.com>
2025-05-28 08:43:43 +0500

    confirmation before sending badge request

--- Backend/src/routes/payment.js
+++ Backend/src/routes/payment.js
@@ -1,6 +1,5 @@
-
-const express=require('express');
-const router=express.Router();
+const express = require('express');
+const router = express.Router();
 const Student = require("../../models/student.js");
 const authjwt = require('../middleware/authjwt.js');
 const Mentor = require("../../models/mentor.js");
@@ -9,19 +8,18 @@ const stripe = require('stripe')(process.env.STRIPE_SECRET);
 
 router.post('/create-checkout-session', authjwt, async (req, res) => {
     console.log('Creating checkout session...');
-    const studentId = req.user.id;         
+    const studentId = req.user.id;
     const mentorId = req.body.mentorId;
 
     try {
         // Fetch mentor and student from DB
         const mentor = await Mentor.findById(mentorId);
         const student = await Student.findById(studentId);
-    
+
         if (!mentor || !student) {
-          return res.status(404).json({ error: 'Mentor or Student not found' });
+            return res.status(404).json({ error: 'Mentor or Student not found' });
         }
-    
-    
+
         // Create a checkout session
         const session = await stripe.checkout.sessions.create({
             payment_method_types: ['card'],
@@ -38,8 +36,17 @@ router.post('/create-checkout-session', authjwt, async (req, res) => {
                 },
             ],
             mode: 'payment',
-            success_url: `http://localhost:3000/success`,
-            cancel_url: `http://localhost:3000/cancel`,
+            // For transferring to mentor accounts
+            /*
+            payment_intent_data: {
+                application_fee_amount: 200, // 10% fee (in cents)
+                transfer_data: {
+                    destination: mentor.accountId, // Mentor's Stripe Connect account ID
+                },
+            },
+            */
+            success_url: "http://localhost:3000/success",
+            cancel_url: "http://localhost:3000/cancel",
         });
 
         res.json({ id: session.id });
@@ -49,4 +56,4 @@ router.post('/create-checkout-session', authjwt, async (req, res) => {
     }
 });
 
-module.exports=router;
+module.exports = router;
